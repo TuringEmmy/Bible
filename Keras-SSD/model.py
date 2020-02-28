c2d3bd1f8e976e3837b419df2842a33f7ae6a48d
TuringEmmy <yonglonggeng@163.com>
2020-02-28 23:19:40 +0800

    SSD MOdel for differetn class

--- Keras-SSD/model.py
+++ Keras-SSD/model.py
@@ -11,7 +11,185 @@
 # @refere   :
 # endregion
 
+import numpy as np
+from keras.models import Model
+from keras.layers import Input
+from keras import backend as K
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from keras_layers.keras_layer_DecodeDetections import DecodeDetections
+from keras_layers.keras_layer_DecodeDetectionsFast import DecodeDetectionsFast
+
+
+def build_model(image_size, n_classes, mode='training', l2_regularization=0.0, min_scale=0.1, max_scale=0.9,
+                scales=None, aspect_ratios_global=[0.5, 1.0, 2.0], aspect_ratios_per_layer=None, two_boxes_for_ar1=True,
+                steps=None, offsets=None, clip_boxes=False, variances=[1.0, 1.0, 1.0, 1.0], coords='centroids',
+                normalize_coords=False, subtract_mean=None, divide_by_stddev=None, swap_channels=False,
+                confidence_thresh=0.01, iou_threshold=0.45, top_k=200, nms_max_output_size=400,
+                return_predictor_sizes=False):
+    """
+    此模型包含7个卷积层，其中4个预测层，预测层从4，5，6和7
+    :param image_size:
+    :param n_classes:
+    :param mode:
+    :param l2_regularization:
+    :param min_scale:
+    :param max_scale:
+    :param scales:
+    :param aspect_ratios_global:
+    :param aspect_ratios_per_layer:
+    :param two_boxes_for_ar1:
+    :param steps:
+    :param offsets:
+    :param clip_boxes:
+    :param variances:
+    :param coords:
+    :param normalize_coords:
+    :param subtract_mean:
+    :param divide_by_stddev:
+    :param swap_channels:
+    :param confidence_thresh:
+    :param iou_threshold:
+    :param top_k:
+    :param nms_max_output_size:
+    :param return_predictor_sizes:
+    :return:
+    """
+    # the number of predictor conv layers in the network
+    n_predictor_layers = 4
+    # account fo rthe background class
+    n_classes += 1
+    # make the internal name shorter
+    l2_reg = l2_regularization
+    img_height, img_width, img_channels = image_size[0], image_size[1], image_size[2]
+
+    ############################################################################
+    # Get a few exceptions out of the way.
+    ############################################################################
+
+    if aspect_ratios_global is None and aspect_ratios_per_layer is None:
+        raise ValueError(
+            "`aspect_ratios_global` and `aspect_ratios_per_layer` cannot both be None. At least one needs to be specified.")
+    if aspect_ratios_per_layer:
+        if len(aspect_ratios_per_layer) != n_predictor_layers:
+            raise ValueError(
+                "It must be either aspect_ratios_per_layer is None or len(aspect_ratios_per_layer) == {}, but len(aspect_ratios_per_layer) == {}.".format(
+                    n_predictor_layers, len(aspect_ratios_per_layer)))
+
+    if (min_scale is None or max_scale is None) and scales is None:
+        raise ValueError("Either `min_scale` and `max_scale` or `scales` need to be specified.")
+    if scales:
+        if len(scales) != n_predictor_layers + 1:
+            raise ValueError("It must be either scales is None or len(scales) == {}, but len(scales) == {}.".format(
+                n_predictor_layers + 1, len(scales)))
+    else:  # If no explicit list of scaling factors was passed, compute the list of scaling factors from `min_scale` and `max_scale`
+        scales = np.linspace(min_scale, max_scale, n_predictor_layers + 1)
+
+    if len(variances) != 4:  # We need one variance value for each of the four box coordinates
+        raise ValueError("4 variance values must be pased, but {} values were received.".format(len(variances)))
+    variances = np.array(variances)
+    if np.any(variances <= 0):
+        raise ValueError("All variances must be >0, but the variances given are {}".format(variances))
+
+    if (not (steps is None)) and (len(steps) != n_predictor_layers):
+        raise ValueError("You must provide at least one step value per predictor layer.")
+
+    if (not (offsets is None)) and (len(offsets) != n_predictor_layers):
+        raise ValueError("You must provide at least one offset value per predictor layer.")
+
+    ############################################################################
+    # Compute the anchor box parameters.
+    ############################################################################
+
+    # Set the aspect ratios for each predictor layer. These are only needed for the anchor box layers.
+    if aspect_ratios_per_layer:
+        aspect_ratios = aspect_ratios_per_layer
+    else:
+        aspect_ratios = [aspect_ratios_global] * n_predictor_layers
+
+    # Compute the number of boxes to be predicted per cell for each predictor layer.
+    # We need this so that we know how many channels the predictor layers need to have.
+    if aspect_ratios_per_layer:
+        n_boxes = []
+        for ar in aspect_ratios_per_layer:
+            if (1 in ar) & two_boxes_for_ar1:
+                n_boxes.append(len(ar) + 1)  # +1 for the second box for aspect ratio 1
+            else:
+                n_boxes.append(len(ar))
+    else:  # If only a global aspect ratio list was passed, then the number of boxes is the same for each predictor layer
+        if (1 in aspect_ratios_global) & two_boxes_for_ar1:
+            n_boxes = len(aspect_ratios_global) + 1
+        else:
+            n_boxes = len(aspect_ratios_global)
+        n_boxes = [n_boxes] * n_predictor_layers
+
+    if steps is None:
+        steps = [None] * n_predictor_layers
+    if offsets is None:
+        offsets = [None] * n_predictor_layers
+
+    ############################################################################
+    # Define functions for the Lambda layers below.
+    ############################################################################
+
+    def identity_layer(tensor):
+        return tensor
+
+    def input_mean_normalization(tensor):
+        return tensor - np.array(subtract_mean)
+
+    def input_stddev_normalization(tensor):
+        return tensor / np.array(divide_by_stddev)
+
+    def input_channel_swap(tensor):
+        if len(swap_channels) == 3:
+            return K.stack(
+                [tensor[..., swap_channels[0]], tensor[..., swap_channels[1]], tensor[..., swap_channels[2]]], axis=-1)
+        elif len(swap_channels) == 4:
+            return K.stack([tensor[..., swap_channels[0]], tensor[..., swap_channels[1]], tensor[..., swap_channels[2]],
+                            tensor[..., swap_channels[3]]], axis=-1)
+
+    ############################################################################
+    # Build the network.
+    ############################################################################
+
+    x = Input(shape=(img_height, img_width, img_channels))
+
+    # 添加网络结构
+
+    if mode == 'training':
+        model = Model(inputs=x, outputs=predictions)
+    elif mode == 'inference':
+        decoded_predictions = DecodeDetections(confidence_thresh=confidence_thresh,
+                                               iou_threshold=iou_threshold,
+                                               top_k=top_k,
+                                               nms_max_output_size=nms_max_output_size,
+                                               coords=coords,
+                                               normalize_coords=normalize_coords,
+                                               img_height=img_height,
+                                               img_width=img_width,
+                                               name='decoded_predictions')(predictions)
+        model = Model(inputs=x, outputs=decoded_predictions)
+    elif mode == 'inference_fast':
+        decoded_predictions = DecodeDetectionsFast(confidence_thresh=confidence_thresh,
+                                                   iou_threshold=iou_threshold,
+                                                   top_k=top_k,
+                                                   nms_max_output_size=nms_max_output_size,
+                                                   coords=coords,
+                                                   normalize_coords=normalize_coords,
+                                                   img_height=img_height,
+                                                   img_width=img_width,
+                                                   name='decoded_predictions')(predictions)
+        model = Model(inputs=x, outputs=decoded_predictions)
+    else:
+        raise ValueError(
+            "`mode` must be one of 'training', 'inference' or 'inference_fast', but received '{}'.".format(mode))
+
+    if return_predictor_sizes:
+        # The spatial dimensions are the same for the `classes` and `boxes` predictor layers.
+        predictor_sizes = np.array([classes4._keras_shape[1:3],
+                                    classes5._keras_shape[1:3],
+                                    classes6._keras_shape[1:3],
+                                    classes7._keras_shape[1:3]])
+        return model, predictor_sizes
+    else:
+        return model
